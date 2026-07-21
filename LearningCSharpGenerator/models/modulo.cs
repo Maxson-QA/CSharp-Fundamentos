3ffee2ddd1b98bb9ffe9045afafb56934820515e
MaxsonDBS <maxsondaniel.work@gmail.com>
2026-07-21 12:07:07 -0300

    Delete LearningCSharpGenerator directory

--- LearningCSharpGenerator/models/modulo.cs
+++
@@ -1,8 +0,0 @@
-namespace LearningCSharpGenerator.Models;
-
-public class Modulo
-{
-    public string Nome { get; set; } = "";
-
-    public List<Topico> Topicos { get; set; } = new();
-}
--- LearningCSharpGenerator/models/topico.cs
+++
@@ -1,8 +0,0 @@
-namespace LearningCSharpGenerator.Models;
-
-public class Topico
-{
-    public string Nome { get; set; } = "";
-
-    public string ArquivoCodigo { get; set; } = "";
-}
